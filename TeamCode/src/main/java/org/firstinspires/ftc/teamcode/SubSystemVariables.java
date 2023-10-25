177a927bf26c330d56b486e786696b44deac3266
inivi <ini.vivek@gmail.com>
2023-10-24 19:35:32 -0500

    Iniyann =========== Wrote some test code to get robot to backstage during autonomous period! + i coded from home so that is also an achievenent :D

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystemVariables.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystemVariables.java
@@ -1,26 +1,34 @@
 package org.firstinspires.ftc.teamcode;
 
 public class SubSystemVariables {
-    public static int INTAKE_LIFT_POS_0 = 143;
-    public static int INTAKE_LIFT_POS_1 = 5;
-    public static int INTAKE_LIFT_POS_2 = 0;
-    public static int INTAKE_LIFT_POS_3 = 800;
+    public static double distToBackboard;
+    public static int headingToBackboard;
 
-    public static double CLAW_ARM_POWER = 0.3;
-    public static int CLAW_ARM_POS_0 = 450;
-    public static int CLAW_ARM_POS_1 = 5;
-    public static int CLAW_ARM_POS_2 = 250;
-    public static int CLAW_ARM_POS_3 = 250;
+    public static final int INTAKE_LIFT_POS_0 = 143;
+    public static final int INTAKE_LIFT_POS_1 = 5;
+    public static final int INTAKE_LIFT_POS_2 = 0;
+    public static final int INTAKE_LIFT_POS_3 = 800;
+
+    public static final double CLAW_ARM_POWER = 0.3;
+    public static final int CLAW_ARM_POS_0 = 450;
+    public static final int CLAW_ARM_POS_1 = 5;
+    public static final int CLAW_ARM_POS_2 = 250;
+    public static final int CLAW_ARM_POS_3 = 250;
 
 
-    public static double INTAKE_LIFT_POWER = 0.4;
+    public static final double INTAKE_LIFT_POWER = 0.4;
 
-    public static double HANG_LIFT_HANG_POWER = 0.5;
-    public static double HANG_LIFT_DROP_POWER = 0.1;
+    public static final double HANG_LIFT_HANG_POWER = 0.5;
+    public static final double HANG_LIFT_DROP_POWER = 0.1;
 
-    public static int HANG_LIFT_POS_DROP = 0;
+    public static final int HANG_LIFT_POS_DROP = 0;
 
-    public static int HANG_LIFT_POS_HANG = -8000;
+    public static final int HANG_LIFT_POS_HANG = -8000;
 
     public static boolean CLAW_OPEN = false;
+    public static enum ALLIANCE_COLOR {BLUE, RED};
+    public static enum ALLIANCE_SIDE {BOTTOM, TOP};
+    public static ALLIANCE_COLOR allianceColor = ALLIANCE_COLOR.BLUE;
+    public static ALLIANCE_SIDE allianceSide = ALLIANCE_SIDE.BOTTOM;
+
 }
